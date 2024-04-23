81de8351b0eed3322a9a0f8c6244307a3006c2ae
Josue-RS <josue.ruiz@wizeline.com>
2024-04-23 17:03:11 -0600

    test4

--- src/main/java/com/vioc/central/motor/repository/VehicleToEngineConfigAcesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vioc.central.motor.repository;
-
-import com.vioc.core.data.repository.ImmutableRepository;
-import com.vioc.pos.vcdb.aces.domain.VehicleToEngineConfigAces;
-
-public interface VehicleToEngineConfigAcesRepository extends ImmutableRepository<VehicleToEngineConfigAces, Integer> {
-
-}
